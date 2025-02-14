244c6e888b954390c98a8d4d3484cdf61479e178
harold <rhey.pisos@gmail.com>
2025-02-14 17:20:48 +0800

    respective pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex flex-col items-center justify-center h-screen">
+    <a href="/staff-list">Staff List</a>
+    <!-- please add your pages here para isa lang tanan then pag navigate lang dayun mo sa inyong respective page -->
+</div>
